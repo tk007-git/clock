4ed436806352f41f839d4a1f01316904cebd420c
Tanmoy karmakar <59302188+IND-tk007@users.noreply.github.com>
2021-04-07 15:15:40 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,4 +6,13 @@ From time import strftime
 Root = tk()
 Root.title("clock")
 
-Label = Label(root, font=("arial"))
+def time():
+  string = strftime('%H:%M:%S %p')
+  label.config(text=string)
+  label.after(1000, time)
+
+Label = Label(root, font=("arial", 80), background = "gray", foreground = "red")
+Lable.pck(anchor='center')
+time()
+
+mainloop()
